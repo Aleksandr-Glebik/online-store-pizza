18ac9a1030331df8dbc5b1be2ac59c7ee3768f1e
Aleksandr-Glebik <glebikaa@mail.ru>
2023-01-12 15:35:03 +0300

    feat: add state for comp Categories, change className style active

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Categories = () => {
   const pizzaCategories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
 
+  const [active, setActive] = useState('Все')
+  // console.log('active', active);
+
   return (
     <div className="categories">
         <ul>
-            {pizzaCategories.map( (name, ind) => (
+            {pizzaCategories && pizzaCategories.map( (name, ind) => (
                 <li
+                  onClick={() => setActive(name)}
                   key={`${name}_${ind}`}
-                  className={name === 'Все' ? 'active' : ''}
+                  className={name === active ? 'active' : ''}
                 >{name}</li>
             ))}
         </ul>
     </div>
   )
-} 
+}
 
 export default Categories
