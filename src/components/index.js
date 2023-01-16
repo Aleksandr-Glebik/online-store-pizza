17be5c528b5175043df71b7b8b61adb54a36a3f3
Aleksandr-Glebik <glebikaa@mail.ru>
2023-01-16 18:29:51 +0300

    feat: add component SortPopup, useState, useEffect, useRef

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import Header from "./Header"
 import Button from "./Button"
 import Categories from "./Categories"
+import SortPopup from "./SortPopup"
 
 export {
-    Header, Button, Categories
+    Header, Button, Categories, SortPopup
 }
 
 // export {default as Button} from './Button'
 // export {default as Header} from './Header'
 // export {default as Categories} from './Categories'
+// export {default as SortPopup} from './SortPopup'
