074d00a8bc4758340dc3bd5cff5ecd4100f26fa8
Aleksandr-Glebik <glebikaa@mail.ru>
2023-01-17 21:58:00 +0300

    feat: add file db.json with pizzas, add comp PizzaBlock and display all pizzas in content block

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,10 @@ import Header from "./Header"
 import Button from "./Button"
 import Categories from "./Categories"
 import SortPopup from "./SortPopup"
+import PizzaBlock from "./PizzaBlock"
 
 export {
-    Header, Button, Categories, SortPopup
+    Header, Button, Categories, SortPopup, PizzaBlock
 }
 
 // export {default as Button} from './Button'
